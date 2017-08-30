71300b7cb5d2d54f6b5ea1ed628ff709bc3441a4
Kharybin <ai.kharybin@gmail.com>
2017-08-30 22:24:14 +0300

    0.02
    Created ConsoleHelper class;
    Created MessageType enum;

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,4 +1,39 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String value = "";
+        try {
+            value = reader.readLine();
+        }
+        catch (IOException e){
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            value = readString();
+        }
+        return value;
+    }
+
+    public static int readInt(){
+        int value = 0;
+        try {
+            value =  Integer.parseInt(readString());
+        }
+        catch (NumberFormatException e){
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            value =  Integer.parseInt(readString());
+        }
+        return value;
+    }
 }
+
