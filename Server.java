8975ddd33cf474fbfaef60b1e70fca65809ce607
Kharybin <ai.kharybin@gmail.com>
2017-08-31 05:23:07 +0300

    0.11
    refactored Server.java class
    added: +sendBroadcastMessage(Message): void

--- Server.java
+++ Server.java
@@ -3,44 +3,40 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        int port;
 
-        System.out.println("Enter port number");
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
-        port = ConsoleHelper.readInt();
-
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("Server started");
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void sendBroadcastMessage(Message message){
+        for (Connection connection: connectionMap.values()) {
             try {
-                serverSocket.close();
-            } catch (IOException e1) {
-                System.out.println("failed to start server, server socket closed");
-                e1.printStackTrace();
+                connection.send(message);
+            }
+            catch (IOException e){
+                System.out.println("cant send message to " + connection.getRemoteSocketAddress());
             }
         }
+    }
 
-        while (true){
-            try {
-                new Handler(serverSocket.accept());
-            } catch (IOException e) {
-                e.printStackTrace();
-                try {
-                    serverSocket.close();
-                } catch (IOException e1) {
-                    System.out.println("failed to accept connection, server socket closed");
-                    e1.printStackTrace();
-                }
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            System.out.println("Сервер запущен");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+                /*Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();*/
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
+
     private static class Handler extends Thread{
         private Socket socket;
 
