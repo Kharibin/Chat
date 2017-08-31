01d361c1ba423776d309103d1f172ed6208b533e
Kharybin <ai.kharybin@gmail.com>
2017-08-31 19:58:11 +0300

    0.42
    Client.java
    
    added +run():void

--- client/Client.java
+++ client/Client.java
@@ -9,19 +9,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Client {
+public class Client extends Thread{
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
+    public static void main(String[] args) {
+        Client client1 = new Client();
+        client1.run();
+    }
+
+    @Override
+    public void run() {
+       SocketThread socketThread = getSocketThread();
+       socketThread.setDaemon(true);
+       socketThread.start();
+       synchronized (this){
+           try {
+               this.wait();
+           } catch (InterruptedException e) {
+               ConsoleHelper.writeMessage("Exception during waiting");
+               return;
+           }
+       }
+       while (clientConnected){
+           ConsoleHelper.writeMessage("connection established");
+           String text = ConsoleHelper.readString();
+           if (text.equals("exit")) break;
+           if (shouldSendTextFromConsole()) try {
+               connection.send(new Message(MessageType.TEXT, text));
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
+    }
+
     protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Enter server address");
         return ConsoleHelper.readString();
     }
 
     protected int getServerPort(){
+        ConsoleHelper.writeMessage("Enter server port");
         return ConsoleHelper.readInt();
     }
 
     protected String getUserName(){
+        ConsoleHelper.writeMessage("Enter user name");
         return ConsoleHelper.readString();
     }
 
