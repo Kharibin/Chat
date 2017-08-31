5dfb1aff29dbb5faa3076ef4ef17ae1a8012c515
Kharybin <ai.kharybin@gmail.com>
2017-08-31 19:00:28 +0300

    0.41
    add main funcionality for client

--- client/Client.java
+++ client/Client.java
@@ -1,10 +1,46 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress(){
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("error sending message, u will be disconnected");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread{
 
