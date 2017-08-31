b51f6207db0fa2f66eae97e24cccee3a325eda10
Kharybin <ai.kharybin@gmail.com>
2017-08-31 04:43:03 +0300

    0.1
    add Server.java class with main functions.

--- Server.java
+++ Server.java
@@ -1,8 +1,51 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
-        
-    }
+        ServerSocket serverSocket = null;
+        int port;
 
+        System.out.println("Enter port number");
+
+        port = ConsoleHelper.readInt();
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started");
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                serverSocket.close();
+            } catch (IOException e1) {
+                System.out.println("failed to start server, server socket closed");
+                e1.printStackTrace();
+            }
+        }
+
+        while (true){
+            try {
+                new Handler(serverSocket.accept());
+            } catch (IOException e) {
+                e.printStackTrace();
+                try {
+                    serverSocket.close();
+                } catch (IOException e1) {
+                    System.out.println("failed to accept connection, server socket closed");
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+    }
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
 }
